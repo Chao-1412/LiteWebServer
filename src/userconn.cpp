7e8e517e1ee8407c16d8b60c6638b8f0e3f5dfcd
chao_1412 <496727059@qq.com>
2025-01-26 14:42:59 +0800

    实现发送逻辑，待测试

--- src/userconn.cpp
+++ src/userconn.cpp
@@ -1,21 +1,50 @@
 #include "userconn.h"
 
+#include <string>
+
 #include <sys/socket.h>
 
 #include <spdlog/spdlog.h>
 
 #include "litewebserver.h"
 
+std::map<HttpCode, UserConn::HandleFunc> UserConn::err_handler_ = {
+    {HttpCode::BAD_REQUEST, err_handler_400},
+    {HttpCode::NOT_FOUND, err_handler_404},
+    {HttpCode::NOT_ALLOWED, err_handler_405},
+    {HttpCode::INTERNAL_SERVER_ERROR, err_handler_500}
+};
+std::map<std::string, std::map<HttpMethod, UserConn::HandleFunc> > UserConn::router_;
+
+void UserConn::register_router(const std::string &path, HttpMethod method,HandleFunc func)
+{
+    router_[path][method] = func;
+}
+
+void UserConn::static_process_in(std::shared_ptr<UserConn> conn)
+{
+    conn->process_in();
+}
+
+void UserConn::static_process_out(std::shared_ptr<UserConn> conn)
+{
+    conn->process_out();
+}
 
 void UserConn::process_in()
 {
+    // 接收数据
     bool ret = recv_from_cli();
     if (ret != true) {
         srv_inst_->modify_conn_event_read(cli_sock_);
         return;
     }
     // 解析收到的数据
-    // 生成返回数据
+    req_parsed_bytes_ += req_.parse(buffer_r_, req_parsed_bytes_);
+    if (!req_.parse_complete()) {
+        srv_inst_->modify_conn_event_read(cli_sock_);
+        return;
+    }
 
     // 返回数据生成后注册epoll写事件，待可写事件触发后
     // 会调用UserConn::process_out，进行处理
@@ -24,31 +53,164 @@ void UserConn::process_in()
 
 void UserConn::process_out()
 {
-    srv_inst_->modify_conn_event_read(cli_sock_);
+    route_path();
+
+    if (!send_to_cli()) {
+        srv_inst_->modify_conn_event_write(cli_sock_);
+    } else {
+        std::string conn_state;
+        rsp_.get_header("Connection", conn_state);
+        if (conn_state == "close") {
+            srv_inst_->disconn_one(cli_sock_);
+        } else {
+            // 如果不是直接断开链接，则重置连接状态
+            conn_state_reset();
+            srv_inst_->modify_conn_event_read(cli_sock_);
+        }
+    }
+}
+
+void UserConn::close_conn()
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    closed_ = true;
 }
 
 //BUG 需要考虑数据很大，撑爆缓冲区满的情况
+//TODO 最好一次性把数据读完，而不是每次epollin读一次，优化性能
 bool UserConn::recv_from_cli()
 {
-    size_t remain_size = buffer_r_.size() - recv_data_size_;
-    char *read_begin = buffer_r_.data() + recv_data_size_;
+    size_t remain_size = conf_->buffer_size_r_ - buffer_r_.size();
+    char *read_begin = buffer_r_.data() + buffer_r_.size();
 
-    ssize_t recv_bytes = recv(cli_sock_, read_begin, remain_size, 0);
+    ssize_t recv_bytes = 0;
+    {
+        std::lock_guard<std::mutex> lock(mtx_);
+        if (closed_) {
+            return false;
+        }
+        recv_bytes = recv(cli_sock_, read_begin, remain_size, 0);
+    }
 
     if (recv_bytes <= 0) {
-        // 不管是 == 0 ，客户端关闭连接
-        // 还是 < 0,
+        // 不管是 == 0：客户端关闭连接
+        // 还是 < 0：
         // EAGAIN EWOULDBLOCK 重试
         // EINTR 系统中断
-        // 都返回false，在main线程epoll池中会对连接进行处理
+        // 先都返回false，
+        // 主线程会根据epoll触发的事件进行处理
+        //（目前考虑到的EAGAIN EWOULDBLOCK EINTR）都有处理，不知道还有没有其他的
         return false;
     } else {
-        recv_data_size_ += recv_bytes;
+        buffer_r_.resize(buffer_r_.size() + recv_bytes);
     }
 
     return true;
 }
 
-void UserConn::parse_data()
+void UserConn::route_path()
 {
+    if (req_.is_bad_req()) {
+        rsp_ = err_handler_[HttpCode::BAD_REQUEST](req_);
+        return;
+    } else {
+        std::string path = req_.get_path();
+        HttpMethod method = req_.get_method();
+        const auto &it_path = router_.find(path);
+        if (it_path == router_.end()) {
+            rsp_ = err_handler_[HttpCode::NOT_FOUND](req_);
+            return;
+        }
+        const auto &call_fn = it_path->second.find(method);
+        if (call_fn == it_path->second.end()) {
+            rsp_ = err_handler_[HttpCode::NOT_ALLOWED](req_);
+            return;
+        }
+
+        rsp_ = call_fn->second(req_);
+    }
+}
+
+bool UserConn::send_to_cli()
+{
+    if (!base_rsp_snd_) {
+        send_base_rsp();
+    }
+
+    if (base_rsp_snd_ && !body_snd_) {
+        send_body();
+    }
+
+    if (base_rsp_snd_ && body_snd_) {
+        return true;
+    }
+
+    return false;
+}
+
+void UserConn::send_base_rsp()
+{
+    ssize_t send_bytes = 0;
+
+    std::lock_guard<std::mutex> lock(mtx_);
+    while (!closed_) {
+        size_t remain_size = rsp_.get_base_rsp().size() - rsp_base_snd_bytes_;
+        const char *snd_beg = rsp_.get_base_rsp().data() + rsp_base_snd_bytes_;
+
+        // 理论上不会出现remain_size < 0的情况
+        if (remain_size <= 0) {
+            base_rsp_snd_ = true;
+            return;
+        }
+        send_bytes = send(cli_sock_, snd_beg, remain_size, 0);
+        if (send_bytes <= 0) {
+            // 主线程会根据epoll触发的事件进行处理
+            //（目前考虑到的EAGAIN EWOULDBLOCK EINTR）都有处理，不知道还有没有其他的
+            SPDLOG_ERROR("send to client failed, code: {}, msg: {}", errno, strerror(errno));
+            return;
+        }
+        rsp_base_snd_bytes_ += send_bytes;
+    }
+}
+
+void UserConn::send_body()
+{
+    ssize_t send_bytes = 0;
+
+    std::lock_guard<std::mutex> lock(mtx_);
+    while (!closed_) {
+        if (rsp_.body_is_file()) {
+
+        } else {
+            size_t remain_size = rsp_.get_body().size() - rsp_body_snd_bytes_;
+            const char *snd_beg = rsp_.get_body().data() + rsp_body_snd_bytes_;
+
+            // 理论上不会出现remain_size < 0的情况
+            if (remain_size <= 0) {
+                body_snd_ = true;
+                return;
+            }
+            send_bytes = send(cli_sock_, snd_beg, remain_size, 0);
+
+            if (send_bytes <= 0) {
+                // 主线程会根据epoll触发的事件进行处理
+                //（目前考虑到的EAGAIN EWOULDBLOCK EINTR）都有处理，不知道还有没有其他的
+                SPDLOG_ERROR("send to client failed, code: {}, msg: {}", errno, strerror(errno));
+                return;
+            }
+            rsp_body_snd_bytes_ += send_bytes;
+        }
+    }
+}
+
+void UserConn::conn_state_reset()
+{
+    buffer_r_.clear();
+    req_.reset();
+    req_parsed_bytes_ = 0;
+    rsp_.reset();
+    base_rsp_snd_ = false;
+    body_snd_ = false;
+    rsp_base_snd_bytes_ = 0;
+    rsp_body_snd_bytes_ = 0;
 }
--- src/userconn.h
+++ src/userconn.h
@@ -2,14 +2,27 @@
 #define SRC_USER_CONN_H_
 
 #include <string>
+#include <functional>
+#include <map>
+#include <memory>
+#include <mutex>
 
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "serverconf.h"
+#include "httpdata.h"
 
 class LiteWebServer;
 
+
 class UserConn {
+public:
+    using HandleFunc = HttpResponse(*)(const HttpRequest&);
+    static void register_router(const std::string &path, HttpMethod method,HandleFunc func);
+    static void static_process_in(std::shared_ptr<UserConn> conn);
+    static void static_process_out(std::shared_ptr<UserConn> conn);
+
 public:
     UserConn(LiteWebServer *const srv_inst,
              const ServerConf *const conf,
@@ -18,24 +31,48 @@ public:
         , conf_(conf)
         , cli_sock_(cli_sock)
         , buffer_r_(std::string(conf_->buffer_size_r_, '\0'))
-        , recv_data_size_(0)
-        , buffer_w_(std::string(conf_->buffer_size_w_, '\0'))
-        , send_data_size_(0)
+        , req_parsed_bytes_(0)
+        , rsp_(req_)
+        , closed_(false)
+        , base_rsp_snd_(false)
+        , body_snd_(false)
+        , rsp_base_snd_bytes_(0)
+        , rsp_body_snd_bytes_(0)
         {}
     void process_in();
     void process_out();
+    void close_conn();
+
+private:
     bool recv_from_cli();
+    void route_path();
     bool send_to_cli();
-    void parse_data();
+    void send_base_rsp();
+    void send_body();
+    /**
+     * @brief 重置连接状态
+     *        当完成一次完整的收发请求后，如果该链接需要继续使用，就需要重置连接状态
+     */
+    void conn_state_reset();
+
+private:
+    static std::map<HttpCode, HandleFunc> err_handler_;
+    static std::map<std::string, std::map<HttpMethod, HandleFunc> > router_;
 
 private:
     LiteWebServer *const srv_inst_;
     const ServerConf *const conf_;
     int cli_sock_;
     std::string buffer_r_;
-    size_t recv_data_size_;
-    std::string buffer_w_;
-    size_t send_data_size_;
+    HttpRequest req_;
+    uint32_t req_parsed_bytes_;
+    HttpResponse rsp_;
+    bool closed_;
+    std::mutex  mtx_;
+    bool base_rsp_snd_;
+    bool body_snd_;
+    uint32_t rsp_base_snd_bytes_;
+    uint32_t rsp_body_snd_bytes_;
 };
 
 #endif  // SRC_USER_CONN_H_
