5c116bcbc252dbcc1481869ced2037ee0526bca7
chao_1412 <496727059@qq.com>
2025-02-01 12:19:15 +0800

    增加新的接口，用于设置nagle算法和reuseaddr

--- src/fdutil.h
+++ src/fdutil.h
@@ -5,6 +5,7 @@
 
 #include <fcntl.h>
 #include <sys/epoll.h>
+#include <netinet/tcp.h> 
 
 
 class FdUtil
@@ -13,7 +14,7 @@ public:
     static int set_nonblocking(int fd);
     static int epoll_add_fd(int epollfd, int fd, uint32_t events);
     /**
-     * 添加fd到epollfd，默认设置为EPOLLONESHOT
+     * @brief 添加fd到epollfd，默认设置为EPOLLONESHOT
      * @param epollfd epoll实例
      * @param fd 需要添加的fd
      * @param events 事件类型
@@ -21,7 +22,7 @@ public:
      */
     static int epoll_add_fd_oneshot(int epollfd, int fd, uint32_t events);
     /**
-     * 修改fd在epollfd中的事件类型，默认设置为EPOLLONESHOT
+     * @brief 修改fd在epollfd中的事件类型，默认设置为EPOLLONESHOT
      * 通过EPOLL_CTL_ADD命令添加的带有EPOLLONESHOT标志的fd，在触发一次后不再触发，
      * 需要EPOLL_CTL_DEL后重新EPOLL_CTL_ADD
      * 或者通过EPOLL_CTL_MOD修改
@@ -32,6 +33,13 @@ public:
      */
     static int epoll_mod_fd_oneshot(int epollfd, int fd, uint32_t events);
     static int epoll_del_fd(int epollfd, int fd);
+    /**
+     * @brief 关闭Nagle
+     *        Nagle 算法，可能导致小数据包延迟发送
+     *        在实时性要求高的应用中可以关闭
+     */
+    static int set_socket_nodelay(int fd);
+    static int set_socket_reuseaddr(int fd);
 };
 
 inline int FdUtil::set_nonblocking(int fd)
@@ -67,6 +75,18 @@ inline int FdUtil::epoll_mod_fd_oneshot(int epollfd, int fd, uint32_t events)
 inline int FdUtil::epoll_del_fd(int epollfd, int fd)
 {
     return epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, NULL);
+}
+
+inline int FdUtil::set_socket_nodelay(int fd)
+{
+    int flag = 1;
+    return setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, (void *)&flag, sizeof(flag));
+}
+
+inline int FdUtil::set_socket_reuseaddr(int fd)
+{
+    int flag = 1;
+    return setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
 }
 
 #endif // SRC_FD_UTIL_
