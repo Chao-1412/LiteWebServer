5aa5a7764d3edde65148b7eeed4881345eb98418
chao_1412 <496727059@qq.com>
2025-02-07 12:52:59 +0800

    更新timer测试

--- tests/test_timer.cpp
+++ tests/test_timer.cpp
@@ -21,56 +21,46 @@ TEST(TimerTest, iswork) {
     /**
      * @brief 测试添加timer
      */
-    TimerManager<MilliSeconds> timer_mgr;
+    TimerManager timer_mgr;
     timer_mgr.add_timer(timer_node1);
     timer_mgr.add_timer(timer_node2);
-    timer_mgr.add_timer(12, now + seconds(3));
-    timer_mgr.add_timer(13);
-    EXPECT_EQ(timer_mgr.queue_size(), 4);
-
-    const TimerNode& node3 = timer_mgr.get_top();
-    EXPECT_EQ(node3.id, 10);
+    timer_mgr.add_timer(12, now + seconds(10));
+    timer_mgr.add_timer(13, now + seconds(7));
+    TimerNode node3 = timer_mgr.get_top();
+    EXPECT_EQ(node3.id, timer_node1.id);
+    timer_mgr.add_timer(1, now);
+    TimerNode node4 = timer_mgr.get_top();
+    EXPECT_EQ(node4.id, 1);
+    EXPECT_EQ(timer_mgr.queue_size(), 5);
 
     /**
      * @brief 测试移除timer
      */
-    timer_mgr.rm_timer(11);
-    EXPECT_EQ(timer_mgr.queue_size(), 3);
+    // 移除timer
+    timer_mgr.rm_timer(1);
+    const TimerNode& top = timer_mgr.get_top();
+    EXPECT_EQ(top.id, 10);
+    EXPECT_EQ(timer_mgr.queue_size(), 4);
     timer_mgr.rm_timer(13);
-    EXPECT_EQ(timer_mgr.queue_size(), 2);
+    EXPECT_EQ(timer_mgr.queue_size(), 3);
+    // 删除不存在的timer，应该不影响程序工作
     timer_mgr.rm_timer(1000);
-    EXPECT_EQ(timer_mgr.queue_size(), 2);
+    EXPECT_EQ(timer_mgr.queue_size(), 3);
 
     /**
      * @brief 测试过期timer
-     *        测试耗时比较久，测过了释掉
      */
-    std::this_thread::sleep_for(seconds(2));
     vector<int> expired;
     timer_mgr.handle_expired_timers(expired);
-    // 因为timer_mgr的最小检查周期是DEF_CHK_MIN_TIME_MS，所以这里应该是0
     EXPECT_EQ(expired.size(), 0);
+    std::this_thread::sleep_for(seconds(1));
+    expired.clear();
+    timer_mgr.handle_expired_timers(expired);
+    EXPECT_EQ(expired.size(), 1);
     EXPECT_EQ(timer_mgr.queue_size(), 2);
-    // std::this_thread::sleep_for(seconds(10));
-    // expired.clear()
-    // timer_mgr.handle_expired_timers(expired);
-    // EXPECT_EQ(expired.size(), 2);
-    // EXPECT_EQ(timer_mgr.queue_size(), 0);
-
-    // 用1秒检查一次的来测试
-    TimerManager<MilliSeconds> timer_mgr2(MilliSeconds(1000));
-    TimerNode timer_node3{1, SteadyClock::now() + seconds(1)};
-    timer_mgr2.add_timer(timer_node3);
-    timer_mgr2.add_timer(11, SteadyClock::now() + seconds(2));
-    timer_mgr2.add_timer(12, SteadyClock::now() + seconds(3));
-    timer_mgr2.add_timer(13);
+    std::this_thread::sleep_for(seconds(1));
     expired.clear();
-    timer_mgr2.handle_expired_timers(expired);
-    EXPECT_EQ(expired.size(), 0);
-    EXPECT_EQ(timer_mgr2.queue_size(), 4);
-    std::this_thread::sleep_for(seconds(3));
-    expired.clear();
-    timer_mgr2.handle_expired_timers(expired);
-    EXPECT_EQ(expired.size(), 3);
-    EXPECT_EQ(timer_mgr2.queue_size(), 1);
+    timer_mgr.handle_expired_timers(expired);
+    EXPECT_EQ(expired.size(), 1);
+    EXPECT_EQ(timer_mgr.queue_size(), 1);
 }
