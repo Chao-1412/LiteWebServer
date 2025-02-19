3d7a9194b7690b47e4f88895a3a0e8467ebfbb5e
chao_1412 <496727059@qq.com>
2025-02-19 15:08:19 +0800

    添加获取文件后缀的函数

--- src/filepathutil.h
+++ src/filepathutil.h
@@ -13,4 +13,23 @@ inline void dir_fix_last_slash(std::string &dir)
     }
 }
 
+/**
+ * @brief 获取文件后缀名
+ *        简单的实现，只判断第一个找到的'.'
+ *        "/ab.cd"，"/ab.tar.gz"，"abd./ab"，"/for/bar.c/"
+ *        这样的路径可能返回奇怪的结果
+ *        不打算修复，对于现在来说够用
+ * @param file_path 文件路径
+ * @return 文件后缀名，如果没有文件后缀名，返回空字符串
+ */
+inline std::string get_file_extension(const std::string &file_path)
+{
+    // 直接返回临时变量，便于RVO优化
+    size_t pos = file_path.rfind('.');
+    if (pos != std::string::npos) {
+        return file_path.substr(pos);
+    }
+    return "";
+}
+
 #endif //SRC_FILEPATHUTIL_H_
